9f98f8d2a5c53d6a68d83f93d4cfe6b8d53c28d3
Carmelo <dellaterra.carmelo@gmail.com>
2022-05-05 15:17:48 +0200

    play command connects and disconnects the bot to the voice channel

--- src/main/java/org/melo/discordbot/utils/commandConfig/commands/CommandPlay.java
+++ src/main/java/org/melo/discordbot/utils/commandConfig/commands/CommandPlay.java
@@ -1,6 +1,8 @@
 package org.melo.discordbot.utils.commandConfig.commands;
 
+import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.ServerVoiceChannel;
+import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.melo.discordbot.utils.commandConfig.Command;
 import org.melo.discordbot.utils.commandConfig.CommandExecutor;
@@ -13,10 +15,19 @@ public class CommandPlay implements CommandExecutor {
             musicName += element + " ";
         } musicName.substring(0, musicName.length()-1);
 
-        event.getChannel().sendMessage("Is the music you're asking me to play "+musicName+"?");
-
-        ServerVoiceChannel channel;
+        event.getChannel().sendMessage("Is the music you're asking me to play \""+musicName+"\"?");
 
 
+        ServerVoiceChannel channel = event.getMessageAuthor().getConnectedVoiceChannel().get();
+        User user = event.getMessageAuthor().asUser().get();
+        event.getChannel().sendMessage("Joining channel : "+channel.getName());
+
+        channel.connect(false, true).thenAccept(audioConnection -> {
+            event.getChannel().sendMessage("I am successfully connected to your channel, "+user.getName());
+            channel.disconnect();
+        }).exceptionally(e -> {
+            e.printStackTrace();
+            return null;
+        });
     }
 }
